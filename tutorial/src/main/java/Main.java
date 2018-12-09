8e1d674e7877e100caf02025dd34c7b64ba1e913
Jeremy Zhang <jeremy.zhang@smartcar.com>
2018-12-08 17:03:58 -0800

    Small Fix

--- tutorial/src/main/java/Main.java
+++ tutorial/src/main/java/Main.java
@@ -16,12 +16,16 @@ public class Main {
 
     get("/login", (req, res) -> {
       // TODO: Authorization Step 1b: Launch Smartcar authentication dialog
+
+      return null;
     });
 
     get("/exchange", (req, res) -> {
       // TODO: Authorization Step 3: Handle Smartcar response
 
       // TODO: Request Step 1: Obtain an access token
+
+      return "";
     });
 
     get("/vehicle", (req, res) -> {
@@ -30,6 +34,8 @@ public class Main {
       // TODO: Request Step 3: Create a vehicle
 
       // TODO: Request Step 4: Make a request to Smartcar API
+
+      return null;
     });
   }
 }
